3c6b646e0382deedd0921371b0144923647205a6
Wilmar Alastre <51450021+walastre64@users.noreply.github.com>
2025-09-17 14:35:38 -0400

    Update conexion.php

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,11 +1,10 @@
 <?PHP
 function conectate(){
 
-		//$serverName = "WILMARALASTRE\MSSQLSERVER2014"; //serverName\instanceName
-		$serverName = "DCBAHIA\MSSQLSERVER2014"; //serverName\instanceName
+		$serverName = "WILMARALASTRE\MSSQLSERVER2014"; //serverName\instanceName		
 		$connectionInfo = array( "Database"		=>"DELIVERY2",
 								 "UID"	   		=>"sa",
-								 "PWD"	   		=>"Ec14312183.-",
+								 "PWD"	   		=>"",
 								 "CharacterSet" =>"UTF-8",
 								 "TrustServerCertificate" => "yes"
 								 );
